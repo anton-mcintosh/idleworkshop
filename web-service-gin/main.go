17ffef0aa75a15b1f8294c7d45db974ef02bd61a
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-11-17 11:10:43 -0800

    starting backend

---
+++ web-service-gin/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "context",
+  "log",
+  "os",
+
+  "go.mongodb.org/mongo-driver/mongo",
+  "go.mongodb.org/mongo-driver/mongo/options",
+  "github.com/joho/godotenv",
+)
+
+var collection *mongo.Collection
+var ctx = context.TODO()
+
+func ()  {
+  err := godotenv.Load(".env")
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+  mongoURI := os.Getenv("MONGO_URI")
+
+  clientOptions := options.Client().ApplyURI(mongoURI)
+  client, err := mongo.Connect(ctx, clientOptions)
+  if err != nil {
+    log.Fatal(err)
+      }
+  err = client.Ping(ctx, nil)
+  if err != nil {
+    log.Fatal(err)
+      }
+}
