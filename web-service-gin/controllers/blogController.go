b39f378a31fdaff864d7431f2f34fc14d0ed17b1
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-11-20 00:03:51 -0800

    :

--- web-service-gin/controllers/blogController.go
+++ web-service-gin/controllers/blogController.go
@@ -9,7 +9,7 @@ import (
   "idleworkshop/website/models"
   "idleworkshop/website/utils"
 )
-var ctx := context.TODO()
+var ctx = context.TODO()
 var collection = client.Database("blogs").Collection("posts")
 func GetPosts(c *gin.Context) {
   
