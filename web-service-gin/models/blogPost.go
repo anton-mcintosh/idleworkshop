e00ffb6436412d1c6d10f5fa005d453f4c009e1e
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-11-21 10:15:12 -0800

    more errors

--- web-service-gin/models/blogPost.go
+++ web-service-gin/models/blogPost.go
@@ -1,8 +1,12 @@
 package models
 
+import (
+  "go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type BlogPost struct {
-  ID      int    `json:"id" bson:"id"`
-  Date    string `json:"date" bson:"date"`
+  ID      primitive.NewObjectID()     `json:"id" bson:"id"`
+  Date    primitive.NewDateTimeFromTime(time.Now()) `json:"date" bson:"date"`
   Title   string `json:"title" bson:"title"`
   Content string `json:"content" bson:"content"`
 }
