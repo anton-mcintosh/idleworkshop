8a69677bd9de78eae0820531b3f73be968cf69ff
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-11-21 10:25:59 -0800

    dat comma dough

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -21,7 +21,7 @@ func main() {
   collection = client.Database("blogs").Collection("posts")
   router := gin.Default()
   router.GET("/posts", func(c *gin.Context) {(controllers.GetPosts(c, collection, ctx))})
-  router.POST("/posts", middleware.AuthMiddleware() func (c *gin.Context) {(controllers.CreatePost(c, collection, ctx))})
+  router.POST("/posts", middleware.AuthMiddleware(), func (c *gin.Context) {(controllers.CreatePost(c, collection, ctx))})
   router.Run("0.0.0.0:8080")
   /*
   newPost := BlogPost{ID: 1, Title: "Testies!", Content: "One, two!"}
