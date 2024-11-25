28ea38e8775e04635944bc175a49e4ac658e07e7
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-11-25 13:43:20 -0800

    trying this again...insanity

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -22,9 +22,14 @@ func main() {
   // access the collection
   collection = client.Database("blogs").Collection("posts")
   router := gin.Default()
+  corsConfig := cors.DefaultConfig()
+  corsConfig.AllowOrigins = []string{"*"}
+  corsConfig.AllowMethods = []string{"GET", "POST"}
+  corsConfig.AllowHeaders = []string{"Authorization"}
+  corsConfig.ExposeHeaders = []string{"Content-Length"}
   router.GET("/get-posts", func(c *gin.Context) {(controllers.GetPosts(c, collection, ctx))})
   router.POST("/posts", middleware.AuthMiddleware(), func (c *gin.Context) {(controllers.CreatePost(c, collection, ctx))})
-  router.Use(cors.Default())
+  router.Use(cors.New(corsConfig))
   router.Run("0.0.0.0:8080")
   /*
   newPost := BlogPost{ID: 1, Title: "Testies!", Content: "One, two!"}
