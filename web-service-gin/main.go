eaf191a23a9c5f4abe2205642616978084a09492
anton-mcintosh <anton86@gmail.com>
2024-11-25 07:22:01 +0000

    for the error, still doesn't work though

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -27,7 +27,7 @@ func main() {
   config.AllowHeaders = []string{"Authorization", "origin", "content-type"}
   config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
   config.ExposeHeaders = []string{"Content-Length"}
-  router.use(cors.New(config))
+  router.Use(cors.New(config))
   router.GET("/posts", func(c *gin.Context) {(controllers.GetPosts(c, collection, ctx))})
   router.POST("/posts", middleware.AuthMiddleware(), func (c *gin.Context) {(controllers.CreatePost(c, collection, ctx))})
   router.Run("0.0.0.0:8080")
