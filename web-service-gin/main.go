0b95df1e91c4e11165ddd8062b0a114729e6695c
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-11-24 23:10:56 -0800

    fixing error

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -23,7 +23,7 @@ func main() {
   collection = client.Database("blogs").Collection("posts")
   router := gin.Default()
   config := cors.DefaultConfig()
-  router.Use(cors.Default())
+  router.Use(config)
   router.GET("/posts", func(c *gin.Context) {(controllers.GetPosts(c, collection, ctx))})
   router.POST("/posts", middleware.AuthMiddleware(), func (c *gin.Context) {(controllers.CreatePost(c, collection, ctx))})
   router.Run("0.0.0.0:8080")
