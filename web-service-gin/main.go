23da2655e6e72649790c6f57de09f651b3a155cf
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-11-25 13:12:36 -0800

    reverting to no cors stuff

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -22,14 +22,6 @@ func main() {
   // access the collection
   collection = client.Database("blogs").Collection("posts")
   router := gin.Default()
-  config := cors.Config{
-    AllowOrigins: []string{"*"},
-    AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-    AllowHeaders:     []string{"Authorization", "Content-Type", "Origin"},
-    ExposeHeaders:    []string{"Content-Length"},
-    AllowCredentials: true,
-  }
-  router.Use(cors.New(config))
   router.GET("/get-posts", func(c *gin.Context) {(controllers.GetPosts(c, collection, ctx))})
   router.POST("/posts", middleware.AuthMiddleware(), func (c *gin.Context) {(controllers.CreatePost(c, collection, ctx))})
   router.Run("0.0.0.0:8080")
