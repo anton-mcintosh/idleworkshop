3b6f0a8bee927489a111e2f47e1498b4f60e4ddb
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-11-25 13:30:19 -0800

    some test prints to figure this out

--- web-service-gin/middleware/auth.go
+++ web-service-gin/middleware/auth.go
@@ -11,6 +11,8 @@ func AuthMiddleware() gin.HandlerFunc {
   return func(c *gin.Context) {
     token := c.GetHeader("Authorization")
     if token != utils.GetVar("API_KEY") {
+      log.Println("Looking for: ", utils.GetVar("API_KEY"))
+      log.Println("got: ", token)
       c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
       c.Abort()
       return
