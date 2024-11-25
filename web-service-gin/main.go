a49c86e256a9050563537cf22cb4913d16cd1345
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-11-24 23:34:00 -0800

    pre-flight checks or something

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -22,11 +22,19 @@ func main() {
   // access the collection
   collection = client.Database("blogs").Collection("posts")
   router := gin.Default()
-  config := cors.DefaultConfig()
-  config.AllowOrigins = []string{"*"}
-  config.AllowHeaders = []string{"Authorization", "origin", "content-type"}
-  config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
-  config.ExposeHeaders = []string{"Content-Length"}
+  config := cors.Config{
+    AllowOrigins: []string{"*"},
+    AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowHeaders:     []string{"Authorization", "Content-Type", "Origin"},
+    ExposeHeaders:    []string{"Content-Length"},
+    AllowCredentials: true,
+  }
+  router.OPTIONS("/*path", func(c *gin.Context) {
+    c.Header("Access-Control-Allow-Origin", "*")
+    c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+    c.Header("Access-Control-Allow-Headers", "Authorization, Content-Type, Origin")
+    c.Status(204) // No Content
+})
   router.Use(cors.New(config))
   router.GET("/posts", func(c *gin.Context) {(controllers.GetPosts(c, collection, ctx))})
   router.POST("/posts", middleware.AuthMiddleware(), func (c *gin.Context) {(controllers.CreatePost(c, collection, ctx))})
