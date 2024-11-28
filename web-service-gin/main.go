03fb27815276ced617cef21439113be55c682408
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-11-28 10:02:13 -0800

    trying this new cors thingy

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -3,11 +3,12 @@ package main
 import (
   "context"
   "net/http"
-  "web-service-gin/utils"
+  "log"
 
 //  "github.com/gin-contrib/cors"
   "github.com/gin-gonic/gin"
   "go.mongodb.org/mongo-driver/mongo"
+  "github.com/jub0bs/cors"
 
   
   "idleworkshop/website/utils"
@@ -24,14 +25,18 @@ func main() {
   // access the collection
   collection = client.Database("blogs").Collection("posts")
   router := gin.Default()
-  /*
-  corsConfig := cors.DefaultConfig()
-  corsConfig.AllowOrigins = []string{"*"}
-  corsConfig.AllowMethods = []string{"GET", "POST"}
-  corsConfig.AllowHeaders = []string{"Authorization"}
-  corsConfig.ExposeHeaders = []string{"Content-Length"}
-  */
-  router.Use(corsMiddleware())
+  // router.Use(corsMiddleware())
+  corsConfig, err := cors.New(cors.Options{
+    Origins: []string{"*"},
+    Methods: []string{"GET", "POST", "OPTIONS"},
+    RequestHeaders: []string{"Authorization"},
+  })
+  if err != nil {
+    log.Fatal(err)
+  }
+  corsConfig.setDebug(true)
+  router.Use(corsConfig)
+
   router.GET("/get-posts", func(c *gin.Context) {(controllers.GetPosts(c, collection, ctx))})
   router.POST("/posts", middleware.AuthMiddleware(), func (c *gin.Context) {(controllers.CreatePost(c, collection, ctx))})
   router.Run("0.0.0.0:8080")
@@ -43,7 +48,7 @@ func corsMiddleware() gin.HandlerFunc {
     c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
     c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
     if c.Request.Method == "OPTIONS" {
-      c.WriteHeader(http.StatusOK)
+      c.AbortWithStatus(http.StatusOK)
       return
     }
     c.Next()
